12608e24c9e57934f731b859068ed3c6fdfc2d75
antataiv <antata_iv@abv.bg>
2015-02-07 02:11:15 +0200

    update

--- JavaBasics/Homework5-Java-Collections/src/P04LongestIncreasingSequence.java
+++ JavaBasics/Homework5-Java-Collections/src/P04LongestIncreasingSequence.java
@@ -1,9 +1,35 @@
 import java.util.Scanner;
 
 public class P04LongestIncreasingSequence {
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
-		
+
+		String[] numbersAsStr = input.nextLine().trim().split("\\s+");
+		int[] numbersArray = new int[numbersAsStr.length];
+		for (int i = 0; i < numbersAsStr.length; i++) {
+			numbersArray[i] = Integer.parseInt(numbersAsStr[i]);
+		}
+		int counter = 1;
+		int bestEndIndex = 0;
+		int bestCount = 1;
+		System.out.print(numbersArray[0]);
+		for (int i = 1; i < numbersArray.length; i++) {
+			if (numbersArray[i] > numbersArray[i - 1]) {
+				System.out.printf(" %d", numbersArray[i]);
+				counter++;
+				if (counter > bestCount) {
+					bestCount = counter;
+					bestEndIndex = i;
+				}
+			} else {
+				System.out.printf("\n%d", numbersArray[i]);
+				counter = 1;
+			}
+		}
+		System.out.printf("\nLongest: ");
+		for (int i = bestEndIndex - bestCount + 1; i <= bestEndIndex; i++) {
+			System.out.printf("%d ", numbersArray[i]);
+		}
 	}
 }
--- JavaBasics/Homework5-Java-Collections/src/P08ExtractEmails.java
+++ JavaBasics/Homework5-Java-Collections/src/P08ExtractEmails.java
@@ -4,16 +4,14 @@ import java.util.regex.Pattern;
 
 public class P08ExtractEmails {
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Enter input text here: ");
 		String text = input.nextLine();
 		
-		//tozi regex ne raboti izcialo pravilno i za syjalenie ne mojah da go izmislia kak da e tochen
-		//v momenta ne hvashta e-maili, kato no-reply@github.com (ne hvashta no- , a samo reply@github.com)
-		//kakto i info-bg@software-university.software.academy (ne hvashta info-)
-		//na koito mu se padne da mi proveriava domashnoto, shte sym mnogo blagodaren, ako v komentara mi napishe kak bi triabavlo da e po-pravilen (ako znae, razbira se) :)
-		String emailMatcher = "([a-z0-9\\.-_]*)@([a-z0-9\\.-]+[a-z0-9])";
+		String emailMatcher = "([-a-z0-9\\.-_]*)@([a-z0-9\\.-]+[a-z0-9])";
+//		String emailMatcher = "\\b[a-z0-9][a-z-_\\.]+@[a-z]+\\.*[a-z\\.-]*\\w";
 		Pattern pattern = Pattern.compile(emailMatcher);
 		Matcher match  = pattern.matcher(text);
 		
